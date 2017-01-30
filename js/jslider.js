34f42ea8043e747be982acc82e71ec572022d965
Lutsman <333333y@gmail.com>
2017-01-31 04:15:23 +0530

    рабочая механика карусельки

--- js/jslider.js
+++ js/jslider.js
@@ -119,10 +119,16 @@ $.fn.JSlider = function (options) {
                     switch ((e.keyCode ? e.keyCode : e.which)) {
                         case 37:   // Left Arrow
                             e.preventDefault();
+                            if (_jslider.isImageMode) {
+                                _jslider.animThumbs('prev');
+                            }
                             _jslider.animSlide('prev');
                             break;
                         case 39:   // Right Arrow
                             e.preventDefault();
+                            if (_jslider.isImageMode) {
+                                _jslider.animThumbs('next');
+                            }
                             _jslider.animSlide('next');
                             break;
                     }
@@ -141,10 +147,16 @@ $.fn.JSlider = function (options) {
                 clearTimeout(_jslider.options.timeout);
                 switch (direction) {
                     case 'left':
+                        if (_jslider.isImageMode) {
+                            _jslider.animThumbs('next');
+                        }
                         _jslider.animSlide('next');
                         break;
 
                     case 'right':
+                        if (_jslider.isImageMode) {
+                            _jslider.animThumbs('prev');
+                        }
                         _jslider.animSlide('prev');
                         break;
                 }
@@ -157,6 +169,9 @@ $.fn.JSlider = function (options) {
         clearTimeout(_jslider.options.timeout);
         if (!_jslider.options.pause) {
             _jslider.options.timeout = setTimeout(function () {
+                if (_jslider.isImageMode) {
+                    _jslider.animThumbs('next');
+                }
                 _jslider.animSlide('next')
             }, _jslider.options.timeout_delay);
         }
@@ -313,10 +328,16 @@ $.fn.JSlider = function (options) {
 
 
         $(_jslider.options.arrows).find('.next').click(function () {
+            if (_jslider.isImageMode) {
+                _jslider.animThumbs('next');
+            }
             _jslider.animSlide('next');
             return false;
         });
         $(_jslider.options.arrows).find('.prev').click(function () {
+            if (_jslider.isImageMode) {
+                _jslider.animThumbs('prev');
+            }
             _jslider.animSlide('prev');
             return false;
         });
@@ -371,16 +392,14 @@ $.fn.JSlider = function (options) {
 
             $prev.click(function () {
                //console.log('prev');
+                _jslider.animThumbs('prev');
                 _jslider.animSlide('prev');
-                _jslider.animThumbs('prev', 1);
             });
             $next.click(function () {
+                _jslider.animThumbs('next');
                 _jslider.animSlide('next');
-                _jslider.animThumbs('next', 1);
                 //console.log('next');
             });
-
-            console.log(_jslider.options.slider_controls.find('.inner'));
         }
 
         for (var k in groups) {
@@ -395,7 +414,7 @@ $.fn.JSlider = function (options) {
 
     };
 
-    this.animThumbs = function (arrow, count) {
+    /*this.animThumbs = function (arrow, count) {
         var $inner = _jslider.options.slider_controls.find('.inner');
         var options = _jslider.options;
         var thumbOptions = _jslider.options.thumbs;
@@ -405,14 +424,34 @@ $.fn.JSlider = function (options) {
 
         if (arrow === 'prev') {
             if (count) {
-                if (count > thumbOptions.firstInView)
-                left = count * thumbOptions.stepWidth;
-                prefix = '+=';
+                if (count > options.slide_num) {
+                    count = (count - options.slide_num) % options.slide_count;
+                    left = (options.slide_count - count - thumbOptions.viewRange) > 0 ?
+                    (options.slide_count - count - thumbOptions.viewRange) * thumbOptions.stepWidth : 0;
+                    prefix = '-';
+
+                    thumbOptions.firstInView = count - thumbOptions.viewRange;
+                    thumbOptions.lastInView = count;
+                } else {
+                    left = count * thumbOptions.stepWidth;
+                    prefix = '+=';
+
+                    thumbOptions.firstInView -= count;
+                    thumbOptions.lastInView -= count;
+                }
             } else {
                 left = 0;
+
+                thumbOptions.firstInView = 0;
+                thumbOptions.lastInView = thumbOptions.viewRange - 1;
             }
         } else if (arrow === 'next') {
             if (count) {
+                if (count + options.slide_num >= options.slide_count) {
+                    count = (count + options.slide_num - options.slide_count) % options.slide_count;
+                    left = (count - thumbOptions.viewRange) * thumbOptions.stepWidth;
+                    prefix = '-';
+                }
                 left = count * thumbOptions.stepWidth;
                 prefix = '-=';
             } else {
@@ -424,7 +463,56 @@ $.fn.JSlider = function (options) {
             {'left': prefix + left},
             speed
         );
-    };
+    };*/
+
+    this.animThumbs = function (arrow) {
+        var $inner = _jslider.options.slider_controls.find('.inner');
+        var options = _jslider.options;
+        var thumbOptions = _jslider.options.thumbs;
+        var speed = _jslider.options.slide_speed;
+        var left;
+        var prefix = '';
+
+        if (arrow === 'prev') {
+            if (options.slide_num > 0) {
+                if (options.slide_num > thumbOptions.firstInView) {
+                    return;
+                }
+
+                left = thumbOptions.stepWidth;
+                prefix = '+=';
+                thumbOptions.firstInView--;
+                thumbOptions.lastInView--;
+            } else if (options.slide_num === 0) {
+                left = (options.slide_count - thumbOptions.viewRange) * thumbOptions.stepWidth;
+                prefix = '-';
+
+                thumbOptions.firstInView = options.slide_count - thumbOptions.viewRange;
+                thumbOptions.lastInView = options.slide_count - 1;
+            }
+        } else if (arrow === 'next') {
+            if (options.slide_num < options.slide_count - 1) {
+                if (options.slide_num < thumbOptions.lastInView) {
+                    return;
+                }
+
+                left = thumbOptions.stepWidth;
+                prefix = '-=';
+                thumbOptions.firstInView++;
+                thumbOptions.lastInView++;
+            } else if (options.slide_num === options.slide_count - 1) {
+                left = 0;
+
+                thumbOptions.firstInView = 0;
+                thumbOptions.lastInView = thumbOptions.viewRange - 1;
+            }
+        }
+
+        $inner.animate(
+            {'left': prefix + left},
+            speed
+        );
+    }; //запускать перед animSlide
 
     this.init(options);
     return this;
